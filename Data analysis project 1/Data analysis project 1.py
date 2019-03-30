a2ad92630633ce024811cdc44b1fbdf8d69d2cb6
Aloise3 <32795834+Aloise3@users.noreply.github.com>
2019-03-30 17:32:24 +0100

    Corrected two typos and cleared up correlations

--- Data analysis project 1/Data analysis project 1.py
+++ Data analysis project 1/Data analysis project 1.py
@@ -1,14 +1,9 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import re
-sns.set_style("darkgrid")
-from sklearn import linear_model
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.svm import SVR
 import statsmodels.formula.api as sm
 
 
@@ -31,34 +26,26 @@ df.drop(columns=['MENA','SSAF',	'LAC','WEOFF','EECA','SEAS','ppidevny', 'ppidevg
 #Long to wide. Not needed as I found out I could take a mean as listed below.
             #df_wide = df.pivot('country', 'year')
             #print(df_wide)
+            
 #Calculating means for 2010-2014
-means = df.groupby('country')['rgdpe','popgr', 'csh_i', 'csh_g', 'pl_i', 'gdpgro', 'pri', 'sec', 'etnony', 'gdpgr', 'gdppc', 'gdpgrwb', 'gdpwb90', 'gdppcwb05', 'tradeqog', 'CorruptionPerception', 'Political Corruption index', 'Control of Corruption', 'GovernmentEffectiveness', 'PoliticalStability', 'RuleofLaw', 'RegulatoryQuality'].mean()
+means = df.groupby('country')['rgdpe','popgr', 'csh_i', 'csh_g', 'pl_i', 'gdpgr', 'pri', 'sec', 'gdpgr', 'gdppc', 'tradeqog', 'CorruptionPerception', 'Political Corruption index', 'Control of Corruption', 'GovernmentEffectiveness', 'PoliticalStability', 'RuleofLaw', 'RegulatoryQuality'].mean()
 print(means)
 #Send to excel so I can verify that there was no screwups
 means.to_excel("Wide_2010-2014.xlsx")
 
-#Correlation Matrix
-def plot_corr(means,size=1000):
-    corr = means.corr()
-    fig, ax = plt.subplots(figsize=(size, size/10))
-    ax.matshow(corr)
-    plt.xticks(range(len(corr.columns)), corr.columns);
-    plt.yticks(range(len(corr.columns)), corr.columns);
-    plt.matshow(means.corr())
-    plt.show()
-plot_corr(means)
-#Correlation matrix for excel
-corr = means.corr('gdpgr', 'popgr', 'csh_i' )
-corr.style.background_gradient(cmap='coolwarm').set_precision(2)
-plt.matshow(means.corr())
-plt.show()
-print(corr)
-corr.to_excel("Correlation Matrix")
-#MAKE SCATTER PLOTS
 
+#MAKE SCATTER PLOTS
+sns.pairplot(means[['rgdpe', 'popgr', 'csh_i', 'sec', 'gdpgr']])
+plt.show()
+
+#Correlation Matrix
+corr = means[['rgdpe', 'popgr', 'csh_i', 'sec', 'pri', 'gdpgr']].corr()
+#corr.style.background_gradient(cmap='coolwarm').set_precision(2)
+sns.heatmap(corr, xticklabels=corr.columns.values, yticklabels=corr.columns.values, annot=True, fmt='.2f')
+#sns.heatmap(corr, vmin=corr.values.min(), vmax=1, square=True, linewidths=0.1, annot=True, annot_kws={"size":8}, fmt='.2f')
+plt.show()
 
 #OLS
-
 result_basic = sm.ols(formula="gdpgr ~ rgdpe + csh_g+ csh_i + pri+ sec + pl_i + popgr ", data=means).fit()
 print(result_basic.summary())
 
