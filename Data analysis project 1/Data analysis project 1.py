89941587df50d3e73f098f2ab8c70f5921718513
Aloise3 <32795834+Aloise3@users.noreply.github.com>
2019-03-31 15:22:03 +0200

    Cleaning

--- Data analysis project 1/Data analysis project 1.py
+++ Data analysis project 1/Data analysis project 1.py
@@ -12,38 +12,60 @@ import statsmodels.formula.api as sm
 df = pd.read_excel(r"C:\Course-in-programming\Data analysis project 1\datasætudvidele21.xlsx")
 print(df)
 
-#Dropper 1950-1958 pga datamangel
+#Dropping 1950-2010 due to inconsistent data
 indexNames = df[ (df['year'] >= 1950) & (df['year'] <= 2009) ].index   
-df.drop(indexNames , inplace=True)
-pd.__version__
-print(df)
-print(type(df))
+reduced_df = df
+reduced_df.drop(indexNames , inplace=True)
+
+print(reduced_df)
+print(type(reduced_df))
 #Dropping irrelevant variables
-df.drop(columns=['MENA','SSAF',	'LAC','WEOFF','EECA','SEAS','ppidevny', 'ppidevgammel',	'laam',	'safrica'])
-
-
+reduced_df.drop(columns=['MENA','SSAF',	'LAC','WEOFF','EECA','SEAS','ppidevny', 'ppidevgammel',	'laam',	'safrica'])
 
 #Long to wide. Not needed as I found out I could take a mean as listed below.
             #df_wide = df.pivot('country', 'year')
             #print(df_wide)
             
 #Calculating means for 2010-2014
-means = df.groupby('country')['rgdpe','popgr', 'csh_i', 'csh_g', 'pl_i', 'gdpgr', 'pri', 'sec', 'gdpgr', 'gdppc', 'tradeqog', 'CorruptionPerception', 'Political Corruption index', 'Control of Corruption', 'GovernmentEffectiveness', 'PoliticalStability', 'RuleofLaw', 'RegulatoryQuality'].mean()
+means = reduced_df.groupby('country')['rgdpe','popgr', 'csh_i', 'csh_g', 'pl_i', 'gdpgr', 'pri', 'sec', 'gdppc', 'tradeqog', 'CorruptionPerception', 'Political Corruption index', 'Control of Corruption', 'GovernmentEffectiveness', 'PoliticalStability', 'RuleofLaw', 'RegulatoryQuality'].mean()
 print(means)
 #Send to excel so I can verify that there was no screwups
 means.to_excel("Wide_2010-2014.xlsx")
 
 
 #MAKE SCATTER PLOTS
-sns.pairplot(means[['rgdpe', 'popgr', 'csh_i', 'sec', 'gdpgr']])
+g1 = means['csh_i']
+g2 = means['popgr']
+g3 = means['gdpgr']
+
+plot_data = (g1, g2, g3)
+colors = ("red", "green", "blue")
+groups = ("Real GDP", "Population growth", "GDP growth")
+ 
+fig = plt.figure()
+ax = fig.add_subplot(10, 10, 10, axisbg="1.0")
+ 
+for plot_data, color, group in zip(plot_data, colors, groups):
+        x, y = plot_data
+        ax.scatter(x, y, alpha=0.8, c=color, edgecolors='none', s=30, label=group)
+ 
+plt.title('Scatter')
+plt.legend(loc=2)
+plt.show()
+
+
+#Pairplot
+#sns.pairplot(means[['rgdpe', 'popgr', 'csh_i', 'sec', 'gdpgr']], dropna=True)
+#plt.show()
+sns.pairplot(means[['popgr', 'csh_i', 'gdpgr']], dropna=True)
 plt.show()
 
 #Correlation Matrix
+plt.title("Figure 2: Correlation of growth factors")
 corr = means[['rgdpe', 'popgr', 'csh_i', 'sec', 'pri', 'gdpgr']].corr()
-#corr.style.background_gradient(cmap='coolwarm').set_precision(2)
-#sns.heatmap(corr, xticklabels=corr.columns.values, yticklabels=corr.columns.values, annot=True, fmt='.2f')
-sns.heatmap(corr, vmin=corr.values.min(), vmax=1, square=True, linewidths=0.1, annot=True, annot_kws={"size":8}, fmt='.2f')
+sns.heatmap(corr, xticklabels=corr.columns.values, yticklabels=corr.columns.values, annot=True, fmt='.2f')
 plt.show()
+
 
 #OLS
 result_basic = sm.ols(formula="gdpgr ~ rgdpe + csh_g+ csh_i + pri+ sec + pl_i + popgr ", data=means).fit()
