2c5a4af7cbc933eb2e75f6ea992ae92428029a7b
Aloise3 <32795834+Aloise3@users.noreply.github.com>
2019-04-01 20:58:42 +0200

    Updates

--- Data analysis project 1/Data analysis project 1.py
+++ Data analysis project 1/Data analysis project 1.py
@@ -3,7 +3,6 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-
 import re
 import statsmodels.formula.api as sm
 
@@ -11,7 +10,6 @@ import statsmodels.formula.api as sm
 
 #Indlæs datasæt. Husk at opdatere stien når du skifter mellem computere.
 df = pd.read_excel(r"C:\Course-in-programming\Data analysis project 1\datasætudvidele21.xlsx")
-print(df)
 
 df.head()
 
@@ -21,7 +19,7 @@ df.head()
 
 #We're focusing on the last 5 years of data. Most countries are covered and we don't have to constantly be aware that a financial crisis happened, even though the world was still in aftermath
 #Dropping 1950-2010 due to inconsistent data on majority of the variables
-indexNames = df[ (df['year'] >= 1950) & (df['year'] <= 2000) ].index   
+indexNames = df[ (df['year'] >= 1950) & (df['year'] <= 2009) ].index   
 reduced_df = df
 reduced_df.drop(indexNames , inplace=True)
 
@@ -30,11 +28,21 @@ print(type(reduced_df))
 
             
 #Calculating means for 2010-2014
-means = reduced_df.groupby('country')['rgdpe','popgr', 'csh_i', 'csh_g', 'pl_i', 'gdpgr', 'pri', 'sec', 'gdppc', 'tradeqog', 'CorruptionPerception', 'Political Corruption index', 'Control of Corruption', 'GovernmentEffectiveness', 'PoliticalStability', 'RuleofLaw', 'RegulatoryQuality','MENA','SSAF','LAC','WEOFF','EECA','SEAS'].mean()
+means = reduced_df.groupby('country')['rgdpe','popgr', 'csh_i', 'csh_g', 'pl_i', 'gdpgr', 'pri', 'sec', 'gdppc', 'tradeqog', 'CorruptionPerception', 'Political Corruption index', 'Control of Corruption', 'GovernmentEffectiveness', 'PoliticalStability', 'RuleofLaw', 'RegulatoryQuality'].mean()
 means.head()
 
 #Send to excel so I can verify that there was no screwups
 means.to_excel("Wide_2010-2014.xlsx")
+
+#Adding empty column to reshape PoliticalStability into a indicator
+#The scale goes from -2.5 to 2.5 by definition. 
+bins_pol = [-2.5, -1.5, -0.5, 0.5, 1.5, 2.5]
+group_names_pol = [1,2,3,4,5]
+means['PolStabInd'] = pd.cut(means['PoliticalStability'], bins_pol, labels=group_names_pol)
+
+#Scatter
+plt.scatter('PoliticalStability', 'gdpgr', data = means)
+plt.show()
 
 #Pairplot
 sns.pairplot(means[['gdppc', 'popgr', 'csh_i', 'sec', 'pri', 'gdpgr']], dropna=True)
@@ -42,18 +50,15 @@ plt.show()
 
 #Correlation Matrix
 plt.title("Figure 2: Correlation of growth factors")
-corr = means[['gdppc', 'popgr', 'csh_i', 'sec', 'pri', 'gdpgr']].corr()
+corr = means[['gdppc', 'popgr', 'csh_i', 'sec', 'pri', 'gdpgr', 'PoliticalStability']].corr()
 sns.heatmap(corr, xticklabels=corr.columns.values, yticklabels=corr.columns.values, annot=True, fmt='.2f')
 plt.show()
-
-
-
 
 #OLS
 result_basic = sm.ols(formula="gdpgr ~ rgdpe + csh_g+ csh_i + pri+ sec + pl_i + popgr ", data=means).fit()
 print(result_basic.summary())
 
-result_investment = sm.ols(formula = "csh_i ~ rgdpe + sec + popgr + csh_g", data=means).fit()
+result_investment = sm.ols(formula = "csh_i ~ sec + pri + popgr + csh_g+ tradeqog", data=means).fit()
 print(result_investment.summary())
 
 
